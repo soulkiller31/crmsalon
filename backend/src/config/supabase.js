bd50d54e72a60797885294118fb6434d8a62d76f
Abhishek <cutncultureunisexsalon@gmail.com>
2026-08-21 16:17:08 +0530

    update

--- backend/src/config/supabase.js
+++ backend/src/config/supabase.js
@@ -1,10 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
+import WebSocket from 'ws';
 import config from '../config/index.js';
 
 const supabase = createClient(config.supabase.url, config.supabase.serviceRoleKey, {
   auth: {
     autoRefreshToken: false,
     persistSession: false,
+  },
+  realtime: {
+    transport: WebSocket,
   },
 });
 
